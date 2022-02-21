dc52ed22593d6f177a3efafa92db5da35dfba473
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-02-21 16:13:55 -0300

    feat: Entendendo e utilizando os props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,27 +1,18 @@
 //import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
 
 function App() {
 
-  function sum(a, b) {
-    return a + b
-  }
-
-  const name = "Gabriel"
-
-  const newName = name.toUpperCase()
-
-  const url = "https://via.placeholder.com/150"
+  const nome = "Ana"
 
   return (
     <div className="App">
-      <h1>Olá Pessoal, meu nome é {newName} e este é o meu projeto em React</h1>
-      <p>Espero que gostem!!!</p>
-      <p>Eu acho que vou gostar de aprender :) </p>
-      <p>A soma entre 2 e 3 é igual à {sum(2,3)} </p>
-      <img src={url} alt="Imagem teste"/>
       <HelloWorld/>
+      <SayMyName nome="Roberto"/>
+      <SayMyName nome="Pedro"/>
+      <SayMyName nome={nome}/> {/* Nesse caso seria Ana */}
     </div>
   );
 }
---
+++ meu-projeto/src/components/SayMyName.js
@@ -0,0 +1,11 @@
+function SayMyName(props) {
+    
+return (
+    <div>
+        <p>Como vai a vida {props.nome}, vai tranquila?</p>
+    </div>
+)
+
+}
+
+export default SayMyName
