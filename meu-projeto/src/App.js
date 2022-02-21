b3516cb60a4e4baed19f0a26c015d660f06e4176
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-02-21 15:28:14 -0300

    feat: Fazendo alterações pequenas no JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+
+function sum(a, b) {
+  return a + b
+}
+
+
 function App() {
+
+const name = "Gabriel"
+
+const newName = name.toUpperCase()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá Pessoal, meu nome é {newName} e este é o meu projeto em React</h1>
+      <p>Espero que gostem!!!</p>
+      <p>Eu acho que vou gostar de aprender :) </p>
+      <p>A soma entre 2 e 3 é igual à {sum(2,3)} </p>
     </div>
   );
 }
