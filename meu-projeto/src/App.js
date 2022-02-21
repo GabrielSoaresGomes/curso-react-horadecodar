5d351be54c67e598850ee4b5d0422d1dc2a24bee
Gabriel Gomes <dev.gabrielsoares221@gmail.com>
2022-02-21 19:55:10 -0300

    feat: Inserindo css no projeto

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from "./components/Frase"
 
 function App() {
 
@@ -9,9 +10,11 @@ function App() {
 
   return (
     <div className="App">
+        <h1>Testando CSS</h1>
       <SayMyName nome="Roberto"/>
       <SayMyName nome="Pedro"/>
       <SayMyName nome={nome}/> {/* Nesse caso seria Ana */}
+        <Frase/>
       <Pessoa nome="João" idade="21" profissao="Engenheiro de Software" foto="https://via.placeholder.com/50" />
     </div>
   );
