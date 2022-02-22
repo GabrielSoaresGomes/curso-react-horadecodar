d5c43fbfb8a46963e3f684d21f590c11f39106e8
Gabriel Gomes <dev.gabrielsoares221@gmail.com>
2022-02-22 12:23:11 -0300

    feat: Usando os eventos

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,26 +1,20 @@
-//import logo from './logo.svg';
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from "./components/Frase"
-import List from "./components/List"
+import Evento from "./components/Evento";
+import Form from "./components/Form";
 
 function App() {
 
-  const nome = "Ana"
+    const nome = "Ana"
 
-  return (
+    return (
 
-    <div className="App">
-        <h1>Testando CSS</h1>
-      <SayMyName nome="Roberto"/>
-      <SayMyName nome="Pedro"/>
-      <SayMyName nome={nome}/> {/* Nesse caso seria Ana */}
-        <Frase/>
-      <Pessoa nome="João" idade="21" profissao="Engenheiro de Software" foto="https://via.placeholder.com/50" />
-        <List/>
-    </div>
-  );
+        <div className="App">
+            <h1>Testando Eventos</h1>
+            <Evento numero={1} />
+            <Evento numero={2} />
+            <Form />
+        </div>
+    );
 }
 
 export default App;
